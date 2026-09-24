0596a0d63f26a2fc25659b130170ffcb45f8d1fa
dweeptulika <dweeptulika@gmail.com>
2026-09-25 01:02:48 +0530

    Point robots sitemap declaration to current live host

--- app/robots.ts
+++ app/robots.ts
@@ -1,1 +1,1 @@
-import type {MetadataRoute} from "next"; export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/"},sitemap:"https://www.dweeptulika.in/sitemap.xml"}}
+import type {MetadataRoute} from "next"; import { SITE_URL } from "@/lib/seo"; export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/"},sitemap:SITE_URL+"/sitemap.xml"}}
