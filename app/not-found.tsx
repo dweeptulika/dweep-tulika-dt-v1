5bba8ab96e6bbef5caf6dcef7e941576a61dd08e
dweeptulika <dweeptulika@gmail.com>
2026-09-25 00:36:46 +0530

    Use Next Link on not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="container">
@@ -5,7 +7,7 @@ export default function NotFound() {
         <div className="kicker">Dweep Tulika</div>
         <h1>Page not found</h1>
         <p className="dek">The story or page you requested could not be found.</p>
-        <p><a href="/">Return to Dweep Tulika</a></p>
+        <p><Link href="/">Return to Dweep Tulika</Link></p>
       </article>
     </div>
   );
